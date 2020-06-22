2a03d343b510a649e21c67a7d6c2102a00ea45e0
Миша <misha@MishaBook.local>
2020-06-22 18:25:02 +0300

    DB added

--- main.py
+++ main.py
@@ -1,8 +1,44 @@
+import json
+import datetime
+from mongoengine import *
+import mongoengine_goodjson as gj
+from urllib.parse import unquote
 from fastapi import FastAPI, Depends
-from models import CalcGetRequest, CalcResponce
+from starlette.responses import JSONResponse
+
+from models import CalcGetRequest, CalcPostRequest, CalcResponce
+
 
 # init FastAPI app
 app = FastAPI()
+
+
+# connecting to DB into Docker container
+connect('mongodb_data_container', username='root',
+        password='rootpassword', authentication_source='admin')
+
+
+# defining DB-schema: 1) math operation body, 2) math operation result, 3) date-time
+class History(gj.Document):
+    operation = StringField(max_length=50)
+    result = StringField(max_length=50)
+    created_at = DateTimeField(
+        required=True, default=str(datetime.datetime.now()))
+
+
+# defining exceptional JSON-response for incorrect DB queries
+@app.exception_handler(Exception)
+async def error_handler(request, exc):
+    return JSONResponse({
+        'detail': f'{exc}'
+    })
+
+
+# showing history of math operations
+@app.get('/history', summary='Get history of operations')
+async def get_history():
+    historyItems = History.objects()
+    return {'responce': json.loads(historyItems.to_json())}
 
 
 # showing math operation and a result
@@ -12,4 +48,39 @@ async def get_calc(query: CalcGetRequest = Depends(CalcGetRequest)):
     responce = {'result': '', 'operation': '', 'uid': ''}
     responce['result'] = eval(params['expression'])
     responce['operation'] = params['expression']
+    saved_operation = History(operation=str(params['expression']), result=str(responce['result'])).save()
+    responce['uid'] = str(saved_operation.id)
+    return responce
+
+
+# saving math operation to DB
+@app.post('/calc', summary='Calc as post method', response_model=CalcResponce)
+async def post_calc(body: CalcPostRequest):
+    params = body.dict()
+    responce = {'result': '', 'operation': '', 'uid': ''}
+
+    if params['operator'] == '*':
+        responce['result'] = params['first'] * params['last']
+        responce['operation'] = str(
+            params['first']) + '*' + str(params['last'])
+
+    elif params['operator'] == '/':
+        responce['result'] = params['first'] / params['last']
+        responce['operation'] = str(
+            params['first']) + '/' + str(params['last'])
+
+    elif params['operator'] == '+':
+        responce['result'] = params['first'] + params['last']
+        responce['operation'] = str(
+            params['first']) + '+' + str(params['last'])
+
+    elif params['operator'] == '-':
+        responce['result'] = params['first'] - params['last']
+        responce['operation'] = str(
+            params['first']) + '-' + str(params['last'])
+
+    saved_operation = History(operation=str(
+        responce['operation']), result=str(responce['result'])).save()
+    responce['uid'] = str(saved_operation.id)
+
     return responce
--- models.py
+++ models.py
@@ -1,10 +1,10 @@
-import re
 from fastapi import Query
-
-from pydantic import BaseModel, validator
+from pydantic import BaseModel, Schema, validator
+import re
 
 
 RE_INT_AND_MATH = re.compile(r'([-+]?[0-9]*\.?[0-9]+[\/\+\-\*])+([-+]?[0-9]*\.?[0-9]+)')
+
 
 class CalcGetRequest(BaseModel):
     expression: str = Query(None, max_length=50)
@@ -14,6 +14,17 @@ class CalcGetRequest(BaseModel):
             return v
         else:
             raise ValueError('only simple math and numbers requiert')
+
+
+class CalcPostRequest(BaseModel):
+    first: int
+    last: int
+    operator: str
+    @validator('operator')
+    def onlySimpleMath(cls, v):
+        if v == '*' or v == '/' or v == '-' or v == '+':
+            return v
+        raise ValueError('only simple math requiert')
 
 
 class CalcResponce(BaseModel):
